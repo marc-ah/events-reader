1268d9feea73c93beb7f93722a2ef5820995eb03
marc-ah <marc@geislingen24.de>
2022-04-28 23:21:58 +0200

    add ssl functionality to scraper

--- lib/scrape.py
+++ lib/scrape.py
@@ -4,6 +4,7 @@ import requests
 import logging
 import sys
 import re
+import ssl
 
 import html2text
 h2t = html2text.HTML2Text()
@@ -23,7 +24,19 @@ def scrape_url(url):
 
     #request website
     try:
-        response = requests.get(url, allow_redirects=True)
+    
+        class TLSAdapter(requests.adapters.HTTPAdapter):
+
+            def init_poolmanager(self, *args, **kwargs):
+                ctx = ssl.create_default_context()
+                ctx.set_ciphers('DEFAULT@SECLEVEL=1')
+                kwargs['ssl_context'] = ctx
+                return super(TLSAdapter, self).init_poolmanager(*args, **kwargs)
+
+        session = requests.session()
+        session.mount('https://', TLSAdapter())
+
+        response = session.get(url, allow_redirects=True)
         content = response.content
         text = response.text
         status = response.status_code
